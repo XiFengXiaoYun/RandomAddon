31497faded680370383b52fa6141650e38c76eb9
xifengxiaoyun <3775538464@qq.com>
2025-09-14 16:24:00 +0800

    FortuneLevel now is an attribute

---
+++ src/main/java/com/xifeng/random_addon/nyx/utils/NyxUtil.java
@@ -0,0 +1,11 @@
+package com.xifeng.random_addon.nyx.utils;
+
+import net.minecraft.world.World;
+
+public class NyxUtil {
+    public World world;
+    public NyxUtil(World world) {
+        this.world = world;
+        //this.lunarEvents.add(0, new HiddenMoon(this));
+    }
+}
