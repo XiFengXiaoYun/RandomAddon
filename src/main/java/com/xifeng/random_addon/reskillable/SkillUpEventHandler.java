058c0154e5dde155fbf84f1b7fd67569773b38e7
xifengxiaoyun <3775538464@qq.com>
2025-11-14 23:45:42 +0800

    Fix a bug from first aid mod

--- src/main/java/com/xifeng/random_addon/reskillable/SkillUpEventHandler.java
+++ src/main/java/com/xifeng/random_addon/reskillable/SkillUpEventHandler.java
@@ -4,7 +4,7 @@ import codersafterdark.reskillable.api.event.LevelUpEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-
+//TODO: 让技能也能提升属性
 public final class SkillUpEventHandler {
     @SubscribeEvent
     public static void onLevelUp(LevelUpEvent.Post evt) {
